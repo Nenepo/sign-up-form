55a01e8c023c9b97c52ca5687e01eb5ba0bb5be1
Nafisa <Nafisafaruna@gmail.com>
2023-08-03 15:57:37 +0100

    feat: adding the javascript for functionality

---
+++ newsletter-sign-up-with-success-message-main/sign-up-form/app.js
@@ -0,0 +1,8 @@
+  // Get the button element by its ID
+  const button = document.getElementById('myButton');
+
+  // Add a click event listener to the button
+  button.addEventListener('click', function() {
+      // Replace 'desired-page.html' with the URL of the page you want to link to
+      window.location.href = 'success.html';
+  });
