68dc561d7b3479ad92d68576ae808f99586a7b9f
Nafisa <Nafisafaruna@gmail.com>
2023-08-05 07:20:39 +0100

    it does nut submit when !valid

--- newsletter-sign-up-with-success-message-main/sign-up-form/app.js
+++ newsletter-sign-up-with-success-message-main/sign-up-form/app.js
@@ -6,13 +6,13 @@
   // Add a click event listener to the button
   button.addEventListener('click', function() {
       // Replace 'desired-page.html' with the URL of the page you want to link to
-      window.location.href = 'success.html';
+      const getEmail = document.getElementById('Email');
+      const validationMessage = ('Valid email required');
+      if(getEmail.checkValidity()){
+        window.location.href = 'success.html';
+     }
+     else{
+      document.innerHTML.getElementById('validationMsg') = validationMessage;
+     }
+      // window.location.href = 'success.html';
   });
-
-  function validate() {
-    const getEmail = document.getElementById('Email');
-    if(!getEmail){
-      
-    }
-      
-  }
