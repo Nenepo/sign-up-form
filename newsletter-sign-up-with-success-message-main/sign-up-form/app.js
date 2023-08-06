9c2d6c59af2cc65cbdd9505ea55df63678fddfa8
Nafisa <Nafisafaruna@gmail.com>
2023-08-06 19:28:56 +0100

    feat: i was able to improve my code

--- newsletter-sign-up-with-success-message-main/sign-up-form/app.js
+++ newsletter-sign-up-with-success-message-main/sign-up-form/app.js
@@ -1,18 +1,59 @@
-//change the page using javascript
+// document.querySelector('form-mail').addEventListener('submit', function(event) {
+//   // event.preventDefault();
+//   const emailInput = document.getElementById('email');
+//   const email = emailInput.value.trim();
+//   const emailValidationMessage = document.getElementById('validationMsg');
+//   const emailSpan = document.getElementById('personalEmail');
+//   if (email === '') {
+//       emailValidationMessage.textContent = 'Please enter your email';
+//       emailInput.classList.add('invalid');
+//       return;
+//   }
 
-// Get the button element by its ID
-  const button = document.getElementById('myButton');
+//   if (!isValidEmail(email)) {
+//       emailValidationMessage.textContent = 'Valid email required';
+//       emailInput.classList.add('invalid');
+//       return;
+//   }
 
-  // Add a click event listener to the button
-  button.addEventListener('click', function() {
-      // Replace 'desired-page.html' with the URL of the page you want to link to
-      const getEmail = document.getElementById('Email');
-      const validationMessage = ('Valid email required');
-      if(getEmail.checkValidity()){
-        window.location.href = 'success.html';
-     }
-     else{
-      document.innerHTML.getElementById('validationMsg') = validationMessage;
-     }
-      // window.location.href = 'success.html';
-  });
+//   else{emailInput.classList.remove('invalid');}
+//   // Set the email address as the content of the span element
+//   emailSpan.textContent = email;
+
+//   // Form submission successful, redirect to success message page
+  
+//   // window.location.href = `success.html?email=${encodeURIComponent(email)}`;
+// });
+
+function isValidEmail(email) {
+  // Use a regular expression to validate email format
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  return emailRegex.test(email);
+}
+
+// Get the button element by its ID (outside the previous function)
+const button = document.getElementById('myButton');
+// Add a click event listener to the button
+button.addEventListener('click', function() {
+  const getEmail = document.getElementById('email');
+  const validationMessageElement = document.getElementById('validationMsg');
+  validationMessageElement.textContent = 'Valid email required';
+  validationMessageElement.style.color = '  hsl(0, 87%, 64%)';
+  validationMessageElement.style.fontFamily=  'Roboto-bold' ;
+  validationMessageElement.style.fontWeight = 400;
+  validationMessageElement.style.fontSize= '0.7rem';
+  if (getEmail.checkValidity()) {
+      window.location.href = 'success.html';
+  } else {
+    validationMessageElement.textContent = 'Valid email required';
+  }
+});
+
+// Add a keydown event listener to the email input
+emailInput.addEventListener('keydown', function(event) {
+  // Check if the key pressed is the Enter key (keyCode 13)
+  if (event.keyCode === 13) {
+    event.preventDefault(); // Prevent the default form submission on pressing Enter
+    button.click(); // Trigger the click event of the submit button
+  }
+});
